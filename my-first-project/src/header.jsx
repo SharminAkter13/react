4f48822d1e84a626359ea516a06977cadb3653cd
SharminAkter13 <sharminakter13092000@gmail.com>
2025-09-18 11:57:59 +0600

    react

---
+++ my-first-project/src/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <h1>Creating and nesting components</h1>
+        </div>
+    );
+};
+
+export default header;
