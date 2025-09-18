b06b4ce85273a1bd8e19a8742d8543b94c0759e9
SharminAkter13 <sharminakter13092000@gmail.com>
2025-09-18 12:20:18 +0600

    react

--- my-first-project/src/App.jsx
+++ my-first-project/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import First from './First';
+import Second from './Second';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello Hi Bye Bye</h1>
+      <First/>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias omnis quaerat, adipisci ipsum nesciunt sunt? Exercitationem excepturi possimus nam dolor corporis fuga, voluptas ea numquam est illo odit itaque? Qui placeat earum ab saepe mollitia, natus tempore ipsa optio, iure quo vero in esse? Culpa accusamus rerum illo aliquam repudiandae et ex. Ullam quae perferendis corporis repellendus minus omnis delectus facilis porro hic, ex aliquam rem, in natus. Voluptates asperiores nihil repudiandae natus architecto dolor quibusdam commodi saepe sapiente, molestiae corporis atque perferendis rem totam voluptas incidunt itaque non eum odio. Quasi iste quisquam maiores iusto aspernatur, iure inventore qui doloribus quos eos adipisci saepe, quis odio eius sit nesciunt corrupti consequuntur ducimus ipsa accusantium rerum eum. Hic explicabo, quasi consequatur temporibus, magni repudiandae debitis mollitia perspiciatis ipsum natus tempora eaque excepturi atque nemo ab tenetur at sapiente dolorem corrupti quis saepe aut officia. Neque obcaecati voluptates saepe est fugit voluptate? Dolores architecto adipisci fuga, accusantium earum rem dignissimos esse nam. Alias vero natus eveniet libero officia sunt quibusdam iusto accusantium ex rem. Consequatur illum id dolore ea quibusdam? Maiores totam sit repellat, deserunt delectus soluta sint aliquam amet, saepe suscipit eum. Blanditiis sequi illum rerum. Quas minus aperiam cupiditate.</p>
+
+      <Second/>
+      
     </div>
   );
 };
