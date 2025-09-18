4da39339dba728986f5f129a2514bcfdc44150fc
SharminAkter13 <sharminakter13092000@gmail.com>
2025-09-18 11:59:35 +0600

    react

---
+++ my-first-project/src/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+        <div>
+            <h1>React 2025 Done By Sharmin Akter</h1>
+        </div>
+    );
+};
+
+export default footer;
