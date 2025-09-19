407c81ab1205103517373cac223e7c58f3a4ff73
SharminAkter13 <sharminakter13092000@gmail.com>
2025-09-19 23:27:14 +0600

    react

---
+++ my-first-project/src/FormData.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const FormData = () => {
+    return (
+        <div>
+            <form>
+                <div>
+                <div classname="mb-3">
+                    <label htmlfor="exampleInputEmail1" classname="form-label">Email address</label>
+                    <input type="email" classname="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div classname="mb-3">
+                    <label htmlfor="exampleInputPassword1" classname="form-label">Password</label>
+                    <input type="password" classname="form-control" id="exampleInputPassword1" />
+                </div>
+                <div classname="mb-3 form-check">
+                    <input type="checkbox" classname="form-check-input" id="exampleCheck1" />
+                    <label classname="form-check-label" htmlfor="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" classname="btn btn-primary">Submit</button>
+                </div>
+            </form>
+
+        </div>
+    );
+};
+
+export default FormData;
