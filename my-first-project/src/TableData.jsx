436c50972216fe4cdd6084600d927277adfb2851
SharminAkter13 <sharminakter13092000@gmail.com>
2025-09-20 00:15:09 +0600

    react

--- my-first-project/src/TableData.jsx
+++ my-first-project/src/TableData.jsx
@@ -1,37 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const TableData = () => {
-    return (
-        <div >
-            <table className="table table-info-subtle table-striped-columns m-5 p-5">
-                <thead>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Email </th>
-                    <th scope="col">Password</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>@mdogdg</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>@fatjhbhus</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>John</td>
-                    <td>@socialff</td>
-                    </tr>
-            </tbody>
-            </table>
-
-        </div>
-    );
+const TableData = ({ rows }) => {
+  return (
+    <div>
+      <table className="table table-info-subtle table-striped-columns m-5 p-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Email</th>
+            <th scope="col">Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.length === 0 ? (
+            <tr>
+              <td colSpan="3" className="text-center">No data yet</td>
+            </tr>
+          ) : (
+            rows.map((row, index) => (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{row.email}</td>
+                <td>{row.password}</td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TableData;
