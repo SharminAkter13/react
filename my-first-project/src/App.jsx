c97f691ee3cf0711591951a222b16cf5c7bf99e7
SharminAkter13 <sharminakter13092000@gmail.com>
2025-09-20 00:18:31 +0600

    react

--- my-first-project/src/App.jsx
+++ my-first-project/src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import TableData from './TableData';
-import Forms from './Forms';
+import React, { useState } from "react";
+import TableData from "./TableData";
+import Forms from "./Forms";
 
 const App = () => {
+  const [rows, setRows] = useState([]);
+  const handleAddRow = (newRow) => {
+    setRows([...rows, newRow]);
+  };
+
   return (
-    <div>
-      <h2>Please Fill-up the Form</h2>
-      <Forms/>
-      <h2>Forms Data</h2>
-      <TableData/>    
+    <div className="container mt-4">
+      <h2 className="mb-3">Please Fill-up the Form</h2>
+      <Forms onAdd={handleAddRow} />
+
+      <h2 className="mt-4">Forms Data</h2>
+      <TableData rows={rows} />
     </div>
   );
 };
