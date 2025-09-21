a3f48fd4f6a94274bd8b8f2f26cc1cfe2b1a93ca
SharminAkter13 <sharminakter13092000@gmail.com>
2025-09-21 12:44:54 +0600

    react

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HomePage from './pages/HomePage';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -10,9 +12,10 @@ import Checkout from './pages/Checkout';
 import Sidebar from './components/Sidebar';
 const App = () => {
   return (
-    <div>
-      <div className='container-fluid'>
+    <div >
+      <div >
         <BrowserRouter>
+        <div className="row">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<About />} />
@@ -22,6 +25,7 @@ const App = () => {
               <Route path="checkout" element={<Checkout />} />
             </Route>
           </Routes>
+          </div>
         </BrowserRouter>
       </div>
       <div>
