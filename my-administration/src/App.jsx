96cdf8c16fbe2c984777b006ab28a3c438adafd6
SharminAkter13 <sharminakter13092000@gmail.com>
2025-09-24 05:37:40 +0600

    react

--- my-administration/src/App.jsx
+++ my-administration/src/App.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import Navbar from './component/Navbar';
-import Profile from './pages/Profile';
-
+import Master from './pages/Master';
+import CreateUser from './pages/user/CreateUser';
+import ManageUser from './pages/user/ManageUser';
+import CreateCategory from './pages/category/CreateCategory';
+import ManageCategory from './pages/category/ManageCategory';
 const App = () => {
-    return (
-        <div>
-           <Navbar/>
-           <BrowserRouter>
-                <Routes>
-                <Route path="/profile" element={<Profile/>} />
-                </Routes>
-            </BrowserRouter>,
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter>
+
+
+      
+    <Routes>
+      <Route path="/" element={<Master />} />
+      <Route path="/add-user" element={<CreateUser/>} />
+      <Route path="/manage-user" element={<ManageUser />} />
+      <Route path="/add-cat" element={<CreateCategory />} />
+      <Route path="/manage-cat" element={<ManageCategory />} />
+    </Routes>
+  </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
