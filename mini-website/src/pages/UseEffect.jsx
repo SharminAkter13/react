981dab0ee356b18a4032ceb0dd8d3ca07a4ca6d6
SharminAkter13 <sharminakter13092000@gmail.com>
2025-10-04 00:18:55 +0600

    react

--- mini-website/src/pages/UseEffect.jsx
+++ mini-website/src/pages/UseEffect.jsx
@@ -2,23 +2,24 @@ import { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
 function UseEffect() {
-  const[count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
 
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]); // <- add the count variable here
 
-  useEffect(() =>{
-    setTimeout(() => {
-        setCount((count) => count + 1);
-    },1000);
-  },[]);
-  
-
-  
-
-  return <h1>I've rendered {count} times !!!</h1>
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  );
 }
 
-  createRoot(document.getElementById("root")).render(
-    <useEffect/>
-);
+createRoot(document.getElementById('root')).render(
+  <UseEffect />
+); 
 
 export default UseEffect;
