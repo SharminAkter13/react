d47430c7d006b014130673de1712c182f690e72a
SharminAkter13 <sharminakter13092000@gmail.com>
2025-10-03 23:38:53 +0600

    react

--- mini-website/src/pages/UseEffect.jsx
+++ mini-website/src/pages/UseEffect.jsx
@@ -2,19 +2,23 @@ import { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
 function UseEffect() {
-  const [count, setCount] = useState(0);
+  const[count, setCount] = useState(0);
 
-  useEffect(() => {
+
+  useEffect(() =>{
     setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-  });
+        setCount((count) => count + 1);
+    },1000);
+  },[]);
+  
 
-  return <h1>I've rendered {count} times!</h1>;
+  
+
+  return <h1>I've rendered {count} times !!!</h1>
 }
 
-createRoot(document.getElementById('root')).render(
-  <UseEffect />
+  createRoot(document.getElementById("root")).render(
+    <useEffect/>
 );
 
 export default UseEffect;
